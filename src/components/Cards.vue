794defdfc1292b0aed5f42067a90614e2791a566
xingbo <751848178@qq.com>
2017-12-04 16:23:43 +0800

    1、添加网站ico图片 2、修改logo背景透明 3、修改最新发布卡片中链接使用vue router

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -8,7 +8,7 @@
 			</a>
 			<ul>
 				<li v-for="item in randomMovieList">
-					<a :href="item.url" target="_blank">{{ item.name }}</a>
+					<router-link :to="item.url">{{ item.name }}</router-link>
 				</li>
 			</ul>
 		</Card>
@@ -31,47 +31,47 @@
 				movieList: [
 					{
 						name: 'The Shawshank Redemption',
-						url: 'https://movie.douban.com/subject/1292052/',
+						url: '/article/1292052/',
 						rate: 9.6
 					},
 					{
 						name: 'Leon:The Professional',
-						url: 'https://movie.douban.com/subject/1295644/',
+						url: '/article/1295644/',
 						rate: 9.4
 					},
 					{
 						name: 'Farewell to My Concubine',
-						url: 'https://movie.douban.com/subject/1291546/',
+						url: '/article/1291546/',
 						rate: 9.5
 					},
 					{
 						name: 'Forrest Gump',
-						url: 'https://movie.douban.com/subject/1292720/',
+						url: '/article/1292720/',
 						rate: 9.4
 					},
 					{
 						name: 'Life Is Beautiful',
-						url: 'https://movie.douban.com/subject/1292063/',
+						url: '/article/1292063/',
 						rate: 9.5
 					},
 					{
 						name: 'Spirited Away',
-						url: 'https://movie.douban.com/subject/1291561/',
+						url: '/article/1291561/',
 						rate: 9.2
 					},
 					{
 						name: 'The Legend of 1900',
-						url: 'https://movie.douban.com/subject/1292001/',
+						url: '/article/1292001/',
 						rate: 9.2
 					},
 					{
 						name: 'WALL·E',
-						url: 'https://movie.douban.com/subject/2131459/',
+						url: '/article/2131459/',
 						rate: 9.3
 					},
 					{
 						name: 'Inception',
-						url: 'https://movie.douban.com/subject/3541415/',
+						url: '/article/3541415/',
 						rate: 9.2
 					}
 				],
