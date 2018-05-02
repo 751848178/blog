35b05ee3b538fdb7408cdcb51802290ce43f993a
751848178@qq.com <751848178@qq.com>
2018-05-02 20:45:09 +0800

    1、提取dateFormate方法到Utils.js文件中

--- src/server/controllers/article.js
+++ src/server/controllers/article.js
@@ -1,4 +1,5 @@
 const articleService = require("../services/articleService");
+const Utils = require("../commons/Utils");
 const fs = require("fs");
 
 class ArticleController{
@@ -16,7 +17,7 @@ class ArticleController{
 		let params = ctx.request.body;
 		let article = {
 			classId: 2,
-			title: `练字：${dateFormat(new Date(), "yyyy年MM月dd日")}`,
+			title: `练字：${Utils.dateFormat(new Date(), "yyyy年MM月dd日")}`,
 			readCount: 0,
 			tags: "1",
 			content: params.article,
@@ -37,26 +38,5 @@ class ArticleController{
 		}
 		ctx.body = resBody;
 	}
-}
-function dateFormat(date, format) {
-	let o = {
-		"M+": date.getMonth() + 1, //month
-		"d+": date.getDate(), //day
-		"h+": date.getHours(), //hour
-		"m+": date.getMinutes(), //minute
-		"s+": date.getSeconds(), //second
-		"q+": Math.floor((date.getMonth() + 3) / 3), //quarter
-		"S": date.getMilliseconds() //millisecond
-	}
-	if(/(y+)/.test(format)) {
-		format = format.replace(RegExp.$1, (date.getFullYear()+"").substr(4 - RegExp.$1.length));
-	}
-
-	for(var k in o) {
-		if(new RegExp("("+ k +")").test(format)) {
-			format = format.replace(RegExp.$1, RegExp.$1.length==1 ? o[k] : ("00"+ o[k]).substr((""+ o[k]).length));
-		}
-	}
-	return format;
 }
 module.exports = new ArticleController();
